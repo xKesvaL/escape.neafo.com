89742265cfeaae0335309ce76e54e813cebbc4bb
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-04-04 16:11:44 +0200

    feat(site-database): add list of users on the admin page

---
+++ web/site/src/routes/admin/users/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+
+ 
+    const {data} = $props();
+    const users = data.users
+
+</script>
+
+<section class="container">
+Salut
+    {#each users as user}
+        <li>{user.email}{user._id}</li>
+    {/each}
+    
+</section>
