68c28de6c2a8bc0f1b008f001199eab32e8c4654
gbihry <98669194+gbihry@users.noreply.github.com>
2024-04-05 09:45:56 +0200

    feat: getI18n function and flag icons

---
+++ web/site/src/lib/utils/functions.ts
@@ -0,0 +1,18 @@
+import * as m from '$paraglide/messages';
+
+export const i18nKeys = Object.keys(m);
+
+export type I18nKey = (typeof i18nKeys)[number];
+
+/**
+ * @description Gets a i18n message dynamically. Use for cases when you have a dynamic parameter, e.g: `locale_${lang}`
+ * @param {I18nKey} key
+ * @param args
+ */
+export const getI18n = (key: I18nKey, args?: { [key: string]: unknown}) => {
+    if (i18nKeys.includes((key))) {
+        return m[key as keyof typeof m](args)
+    }
+
+    return key;
+}
