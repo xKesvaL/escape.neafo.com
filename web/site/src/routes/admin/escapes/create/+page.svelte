d1f9377002c624e786f62dee96a33664b88bbfe7
xKesvaL <djorden68000@gmail.com>
2024-04-05 19:15:42 +0200

    feat(escape-creation): spacing, i18n and time input styling

--- web/site/src/routes/admin/escapes/create/+page.svelte
+++ web/site/src/routes/admin/escapes/create/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {Field, Fieldset, Control, Label, FieldErrors, Button} from '$lib/components/ui/form';
+    import {Button, Control, Field, FieldErrors, Fieldset, Label} from '$lib/components/ui/form';
     import {Input} from '$lib/components/ui/input';
     import {Textarea} from "$lib/components/ui/textarea";
     import {Switch} from "$lib/components/ui/switch";
@@ -7,8 +7,8 @@
     import {superForm} from 'sveltekit-superforms';
     import {zodClient} from 'sveltekit-superforms/adapters';
     import {Checkbox} from "$lib/components/ui/checkbox";
-    import { availableLanguageTags } from '$paraglide/runtime';
-    import { ESCAPE } from '@repo/global-config';
+    import {availableLanguageTags} from '$paraglide/runtime';
+    import {ESCAPE} from '@repo/global-config';
     import replaceSpecialCharacter from 'replace-special-characters'
     import type {Locale} from "$lib/config/brand";
     import * as m from '$paraglide/messages';
@@ -42,8 +42,8 @@
 <section class="container py-12 flex flex-col gap-10">
     <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Admin - Escape - Creation</h1>
     <form class="flex flex-row gap-8" method="POST" use:enhance>
-        <div class="flex flex-col gap-6 w-full">
-            <div class="flex flex-row gap-8">
+        <div class="flex flex-col gap-4 w-full">
+            <div class="flex flex-row gap-4">
                 <Field {form} class="w-full" name="name">
                     <Control let:attrs>
                         <Label>{m.name()}</Label>
@@ -66,8 +66,8 @@
                 </Control>
                 <FieldErrors/>
             </Field>
-            <Fieldset {form} name="langs">
-                <div class="text-sm font-medium my-2">Language(s)</div>
+            <Fieldset class="space-y-2" {form} name="langs">
+                <div class="text-sm font-medium">{m.escape_langs()}</div>
                 <div class="flex flex-row gap-4">
                     {#each availableLanguageTags as lang}
                         <div class="flex flex-row space-x-2 items-center">
@@ -101,10 +101,11 @@
                     {/each}
                 </div>
             </Fieldset>
-            <div>
+            <div class="space-y-2">
                 <div class="text-sm font-medium">{m.difficulty()}</div>
-                <div class="flex items-center gap-4 my-2">
+                <div class="flex items-center gap-4">
                     {#each ESCAPE.difficulties as difficulty}
+                        <!--                         Easy = success, Medium = primary, Hard = destructive-->
                         {@const color = difficulty === 'easy' ? 'success' : difficulty === 'medium' ? 'primary' : 'destructive'}
                         <Button
                                 type="button"
@@ -122,10 +123,11 @@
                     {/each}
                 </div>
             </div>
-            <div>
+            <div class="space-y-2">
                 <div class="text-sm font-medium">{m.escape_puzzle_difficulty()}</div>
-                <div class="flex items-center gap-4 my-2">
+                <div class="flex items-center gap-4">
                     {#each ESCAPE.puzzle as puzzle}
+                        <!--                        Beginner = info, Intermediate = success, Advanced = primary, Expert = destructive-->
                         {@const color = puzzle === 'beginner' ? 'info' : puzzle === 'intermediate' ? 'success' : puzzle === 'advanced' ? 'primary' : 'destructive'}
                         <Button
                                 type="button"
@@ -143,11 +145,11 @@
                     {/each}
                 </div>
             </div>
-            <div class="flex flex-row gap-8 items-center">
+            <div class="flex flex-row gap-4 items-center">
                 <Field {form} class="w-full" name="time">
                     <Control let:attrs>
                         <Label>{m.time()}</Label>
-                        <Input {...attrs} bind:value={$formData.time} type="time"/>
+                        <Input {...attrs} bind:value={$formData.time} class="input-time" type="time"/>
                     </Control>
                     <FieldErrors/>
                 </Field>
@@ -162,14 +164,14 @@
                     <Control let:attrs>
                         <div class="flex flex-row items-center space-x-2">
                             <Switch {...attrs} bind:checked={$formData.public}/>
-                            <Label>{m.escape_public()}</Label>
+                            <Label>{m.public_()}</Label>
                         </div>
 
                     </Control>
                     <FieldErrors/>
                 </Field>
             </div>
-            <div class="flex flex-row gap-8">
+            <div class="flex flex-row gap-4">
                 <Field {form} class="w-full" name="address">
                     <Control let:attrs>
                         <Label>{m.address()}</Label>
@@ -184,7 +186,7 @@
                     </Control>
                     <FieldErrors/>
                 </Field>
-                <Field {form} class="w-full" name="postal_code">
+                <Field class="w-full max-w-24" {form} name="postal_code">
                     <Control let:attrs>
                         <Label>{m.postal_code()}</Label>
                         <Input placeholder="68940" {...attrs} bind:value={$formData.postal_code} type="number"/>
