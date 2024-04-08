2afc58e5109e7a1b58c9ffb7fe6c0e7fdf5bb0ef
gbihry <98669194+gbihry@users.noreply.github.com>
2024-04-08 17:04:00 +0200

    feat: add back-end to foreach on the escape game in the db

--- web/site/src/routes/admin/escapes/+page.svelte
+++ web/site/src/routes/admin/escapes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import EscapeCard from '$lib/components/admin/EscapeCard.svelte'
 
+    const {data} = $props();
+
+    const games = data.escapes;
 </script>
 
-<EscapeCard></EscapeCard>
+{#each games as game}
+    <EscapeCard name="{games.name}"></EscapeCard>
+{/each}
