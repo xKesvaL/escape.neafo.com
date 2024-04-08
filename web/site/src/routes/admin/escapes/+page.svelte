f9623a05fc9d6e9d1d34780b474d64bf683b7914
Guillaume Bihry <bihryguillaume@gmail.com>
2024-04-08 21:49:15 +0200

    feat: dynamic escape card and front

--- web/site/src/routes/admin/escapes/+page.svelte
+++ web/site/src/routes/admin/escapes/+page.svelte
@@ -3,9 +3,14 @@
 
     const {data} = $props();
 
-    const games = data.escapes;
+    const escapes = data.escapes;
 </script>
 
-{#each games as game}
-    <EscapeCard name="{games.name}"></EscapeCard>
-{/each}
+<div class="container pb-12 flex flex-col gap-4">
+    <h2>List</h2>
+    <div class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-items-center gap-4">
+        {#each escapes as escape}
+            <EscapeCard {escape} />
+        {/each}
+    </div>
+</div>
