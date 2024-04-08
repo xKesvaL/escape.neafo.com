827a4dd9d2873291238ff9525791b2ee9b22e6ee
Guillaume Bihry <bihryguillaume@gmail.com>
2024-04-08 21:50:10 +0200

    feat: front card

--- web/site/src/lib/components/admin/EscapeCard.svelte
+++ web/site/src/lib/components/admin/EscapeCard.svelte
@@ -10,12 +10,10 @@
     const {escape}: Props = $props();
 </script>
 
-<div class="flex bg-transparent bg-accent-foreground rounded-xl  flex-col w-60  p-4 gap-6 shrink-0">
-    <div class="card absolute w-60 h-96">
-
-    </div>
+<div class="flex bg-transparent bg-accent-foreground rounded-2xl relative flex-col p-6 gap-6 shrink-0 max-w-96 overflow-hidden bg-card/50">
+    <enhanced:img class="absolute w-[150%] h-[150%] blur-3xl opacity-50 -z-10" src="$assets/escapes/vinea_flamara.png?w=300" alt="escape_image" style:view-transition-name="escape-img" />
     <div class="rounded-xl">
-        <enhanced:img src="$assets/escapes/vinea_flamara.png?w=200" alt="escape_image" style:view-transition-name="escape-img" />
+        <enhanced:img class="w-full" src="$assets/escapes/vinea_flamara.png?w=300" alt="escape_image" style:view-transition-name="escape-img" />
     </div>
     <div class="flex flex-col gap-1">
         <p class="text-foreground text-2xl">{escape.name}</p>
@@ -24,15 +22,8 @@
             <p class=" text-l">{escape.city}</p>
         </div>
     </div>
-    <div class="flex gap-4 z-10 w-full">
+    <div class="flex gap-6 w-full">
         <Button class="w-full">Edit</Button>
         <Button class="w-full" variant="destructive">Disable</Button>
     </div>
 </div>
-
-<style>
-    .card{
-        background: linear-gradient(0deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.30) 100%), url("../../../assets/escapes/vinea_flamara.png") lightgray 50% / cover no-repeat;
-        filter: blur(46px);
-    }
-</style>
