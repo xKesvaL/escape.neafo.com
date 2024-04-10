03d8ae430bc15cbc5daea2241521019e81eed5f3
xKesvaL <djorden68000@gmail.com>
2024-04-10 11:47:58 +0200

    feat: migrate back to svelte 4 and add filepond

--- packages/schemas/mongo.ts
+++ packages/schemas/mongo.ts
@@ -55,8 +55,9 @@ export const SessionSchema = new Schema(
 );
 
 export const ImageSchema = new Schema({
-    data: Buffer,
-    content_type: String,
+	data: Buffer,
+	content_type: String,
+	id: String,
 });
 
 export const EscapeSchema = new Schema<Escape>(
@@ -104,11 +105,11 @@ export const EscapeSchema = new Schema<Escape>(
 			required: true,
 		},
 		postal_code: {
-			type: Number,
+			type: String,
 			required: true,
 		},
 		price: {
-			type: Number,
+			type: String,
 			required: true,
 		},
 		public: {
@@ -129,4 +130,3 @@ export const EscapeSchema = new Schema<Escape>(
 		},
 	},
 );
-
