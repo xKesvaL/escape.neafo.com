1fc0e814275fdec9a4b0313f31deacde30e72413
xKesvaL <djorden68000@gmail.com>
2024-04-10 19:45:17 +0200

    fix: form-auto fill and other fields

--- web/site/src/routes/admin/users/[userId]/edit/+page.server.ts
+++ web/site/src/routes/admin/users/[userId]/edit/+page.server.ts
@@ -1,22 +1,25 @@
-import { superValidate } from 'sveltekit-superforms';
-import { type User, userEditZodSchema } from '@repo/schemas/zod';
-import { zod } from 'sveltekit-superforms/adapters';
-import type { PageServerLoad, Actions } from './$types.js';
-import { getDatabaseConnection } from '$lib/server/db';
-import { fail } from '@sveltejs/kit';
+import { getDatabaseConnection } from "$lib/server/db";
+import { type User, userEditZodSchema } from "@repo/schemas/zod";
+import { fail } from "@sveltejs/kit";
+import { superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import type { Actions, PageServerLoad } from "./$types.js";
 
 export const load: PageServerLoad = async ({ params }) => {
 	const userId = params.userId;
 
 	const mongoose = await getDatabaseConnection();
-	const userModel = mongoose.model<User>('User');
+	const userModel = mongoose.model<User>("User");
 
 	const user = await userModel.findOne({ _id: userId }).lean().select({
-		email: true
+		email: true,
+		firstname: true,
+		lastname: true,
 	});
+
 	return {
 		user: user,
-		form: await superValidate(zod(userEditZodSchema))
+		form: await superValidate(user, zod(userEditZodSchema)),
 	};
 };
 
@@ -26,35 +29,40 @@ export const actions: Actions = {
 
 		if (!form.valid) {
 			return fail(400, {
-				form
+				form,
 			});
 		}
 
 		const userId = event.params.userId;
-		const { email } = form.data;
+		const { email, firstname, lastname } = form.data;
 
 		const mongoose = await getDatabaseConnection();
 
-		const userModel = mongoose.model<User>('User');
-		const user = await userModel.findOne({ _id: userId }).select({ email: true }).exec();
+		const userModel = mongoose.model<User>("User");
+		const user = await userModel
+			.findOne({ _id: userId })
+			.select({ email: true })
+			.exec();
 
 		if (!user) {
 			return fail(400, {
-				error: 'No user'
+				error: "No user",
 			});
 		}
 
 		if (await userModel.exists({ email })) {
 			return fail(400, {
 				form,
-				error: 'Email already in use'
+				error: "Email already in use",
 			});
 		}
 
 		user.email = email;
+		user.firstname = firstname;
+		user.lastname = lastname;
 
 		await user.save();
 
 		return true;
-	}
+	},
 };
