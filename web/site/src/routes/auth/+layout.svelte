930c47c730a2768f5f69b17b76213f1c99ed5b8f
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-04-11 11:53:41 +0200

    fix: make the login and register pages works

---
+++ web/site/src/routes/auth/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+    import * as Tabs from "$lib/components/ui/tabs";
+    import { goto } from '$app/navigation';
+	import { route } from '$lib/ROUTES.js';
+</script>
+
+<section class="grid grid-cols-[3fr,2fr] gap-8 container p-8">
+    <div class="">
+        <enhanced:img class="w-full h-full" src="$assets/auth/login_img.png" alt="login_img">
+    </div>
+    <div class="flex flex-col items-center text-center gap-4">
+        <Tabs.Root value="account">
+            <Tabs.List class="p-2 h-auto gap-2 min-w-52 bg-muted/75 backdrop-blur-xl backdrop-saturate-150">
+                <Tabs.Trigger class="px-4 py-3 flex-1" on:click={() => goto(route("/auth/login"))}  value="password">Login</Tabs.Trigger>  
+                
+                <Tabs.Trigger class="px-4 py-3 flex-1" on:click={() => goto(route("/auth/register"))} value="account">Register</Tabs.Trigger>
+            </Tabs.List>
+        </Tabs.Root>   
+
+        <div class="">
+            <slot />
+        </div>
+    </div>
+</section>
--- web/site/src/routes/auth/login/+page.svelte
+++ web/site/src/routes/auth/login/+page.svelte
@@ -4,13 +4,11 @@
     import {userRegisterZodSchema} from '@repo/schemas/zod';
     import {superForm} from 'sveltekit-superforms';
     import {zodClient} from 'sveltekit-superforms/adapters';
+    import hand from'site/src/assets/auth/hand.svg';
     import google_logo from 'site/src/assets/auth/google_logo.svg';
     import facebook_logo from 'site/src/assets/auth/facebook_logo.svg';
     import twitter_logo from 'site/src/assets/auth/twitter_logo.svg';
-    import hand from'site/src/assets/auth/hand.svg';
-    import login_img from'site/src/assets/auth/login_img.png';
-   
-  
+    
   
     
 
@@ -23,56 +21,45 @@
 
     const {form: formData, enhance} = form;
 
-    
 </script>
 
-<section class="flex flex-row py-4">
-<div class="w-[120%] h-[100%]">
-    <img src={login_img} alt="login_img">
-</div>
-<section class="container flex flex-col items-center justify-center text-center gap-2 py-8 ">
-  
-    <div class="flex flex-row items-center gap-2">
-        <img src={hand} alt="hand">
-        <h1 class="text-5xl font-bold py-2">Welcome back</h1>
-    </div>
-    <form method="POST" class=" w-2/3" use:enhance>
-        <Field {form} class="text-start " name="email">
-            <Control let:attrs>
-                <Label class="text-lg">Email</Label>
-                <Input class="bg-white font-black" {...attrs} bind:value={$formData.email} placeholder="email@gmail.com"/>
-            </Control>
-            <FieldErrors/>
-        </Field>
-        <Field {form} class="text-start" name="password">
-            <Control let:attrs>
-                <Label class="text-lg">Password</Label>
-                <Input class="bg-white font-black" {...attrs} bind:value={$formData.password} placeholder="Coolpassword123"/>
-            </Control>
-            <FieldErrors/>
-        </Field>
-        <a href="/">
-            <p class="text-muted-foreground underline text-end">Forgot your password ?</p>
-        </a>
-        <Button class="bg-gradient-to-r from-orange-500 to-primary w-full text-2xl font-medium py-2 text-center my-2 mt-4">Login</Button>
-        <div class="mt-10">
-            <div class="flex justify-center items-center self-stretch border-t-2 border-muted-foreground">
-                <span class="bg-background relative -top-5 px-4 text-muted-foreground my-2">OR</span>
-            </div>
-        </div>
 
-        <div class="flex flex-row gap-2">
-            <div class="bg-white w-full justify-center p-2 rounded-md flex flex-row items-center text-center gap-1">
-            <img src={google_logo} alt="google_logo">
-            <span class="text-black font-semibold text-base">Login with Google</span>
-            </div>
-            <div class="bg-white w-fit p-2 rounded-md">
-                <img src={facebook_logo} alt="facebook_logo">
-            </div>
-            <div class="bg-white w-fit p-2 rounded-md">
-                <img src={twitter_logo} alt="twitter_logo">
-            </div>
-        </div>
-    </form>
-</section>
-</section>
+<div class="flex flex-row items-center gap-2">
+    <img src={hand} alt="hand">
+    <h1 class="text-5xl font-bold">Join us now   </h1>
+</div>
+<form method="POST" class="flex flex-col gap-4 max-w-xl" use:enhance>
+    <Field {form} class="text-start " name="email">
+        <Control let:attrs>
+            <Label class="text-lg">Email</Label>
+            <Input class="bg-white font-black" {...attrs} bind:value={$formData.email} placeholder="email@gmail.com"/>
+        </Control>
+        <FieldErrors/>
+    </Field>
+    <Field {form} class="text-start" name="password">
+        <Control let:attrs>
+            <Label class="text-lg">Password</Label>
+            <Input class="bg-white font-black" {...attrs} bind:value={$formData.password} placeholder="Coolpassword123"/>
+        </Control>
+        <FieldErrors/>
+    </Field>
+    <a href="/">
+        <p class="text-muted-foreground underline text-end">Forgot your password ?</p>
+    </a>
+    <Button class="bg-gradient-to-r from-orange-500 to-primary w-full text-2xl font-medium py-2 text-center my-2 mt-4">Login</Button>
+
+    <div class="flex flex-row gap-2">
+        <Button class="bg-white w-full justify-center p-2 rounded-md flex flex-row items-center text-center gap-1">
+        <img src={google_logo} alt="google_logo">
+        <span class="text-black font-semibold text-base">Login with Google</span>
+        </Button>
+        <Button class="bg-white w-fit p-2 rounded-md">
+            <img src={facebook_logo} alt="facebook_logo">
+        </Button>
+        <Button class="bg-white w-fit p-2 rounded-md">
+            <img src={twitter_logo} alt="twitter_logo">
+        </Button>
+    </div> 
+
+
+</form>
