f318d28ef0fe9bdcdc16c70458ef8c7239ef2b1a
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-04-12 16:11:31 +0200

    feat: add terms and condition page

---
+++ web/site/src/lib/containers/legal/LegalContainer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { LegalProps } from '$lib/utils/types';
+
+    export let conditions: Array<LegalProps> = [];
+
+    export let title: string;    
+
+    export let update: string;    
+
+</script>
+
+<section class="container flex flex-row">
+    <div class="flex flex-col gap-4">
+        <h1 class="text-5xl font-extrabold">{title}</h1>
+        <span class="text-lg text-muted-foreground">{update}</span>
+            <div class="flex flex-col gap-4 ">
+                {#each conditions as condition}
+                    <span class="text-white font-semibold text-3xl pb-2">{condition.title}</span>
+                    <div class="text-base font-normal"> {condition.description} </div>
+                {/each}
+            </div>
+    </div>
+
+    <div class="flex flex-col gap-3 p-3">
+        {#each conditions as condition}
+            <span>{condition.title}</span>
+        {/each}
+    </div>
+
+</section>
