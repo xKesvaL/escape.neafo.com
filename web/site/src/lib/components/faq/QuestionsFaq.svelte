fd1b6fc9e00f80eecbc9ce9801174960050615fd
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-04-12 09:55:47 +0200

    feat: add the questions on the faq page

---
+++ web/site/src/lib/components/faq/QuestionsFaq.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { FAQ } from '$lib/utils/types';
+
+    export let faqs: Array<FAQ> = [];
+
+</script>
+
+<div class="flex flex-col gap-4">
+    <span> </span>
+        <div class="flex flex-col gap-2 ">
+            {#each faqs as faq}
+            <div> {faq.question} </div>
+            {/each}
+        </div>
+</div>
