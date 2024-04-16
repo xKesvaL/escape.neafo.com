09ddc3a13e74afa8b715f8786e7fdafd9b76e1e9
Guillaume Bihry <bihryguillaume@gmail.com>
2024-04-16 21:33:45 +0200

    feat: front jobs / career

---
+++ web/site/src/lib/components/icons/IconActivity.svelte
@@ -0,0 +1,13 @@
+<svg  xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="icon icon-tabler icons-tabler-outline icon-tabler-activity text-primary">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M3 12h4l3 8l4 -16l3 8h4" />
+</svg>
