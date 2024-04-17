134d269f8307605c63b26eb9cc173e5b9a0d716f
xKesvaL <djorden68000@gmail.com>
2024-04-17 10:33:23 +0200

    fix: icons in contact

--- web/site/src/routes/contact/+page.svelte
+++ web/site/src/routes/contact/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
- import IconBrandFacebook from '$lib/components/icons/IconBrandFacebook.svelte';
- import IconBrandInstagram from '$lib/components/icons/IconBrandInstagram.svelte';
- import IconPhone from '$lib/components/icons/IconPhone.svelte';
- import IconMail from '$lib/components/icons/IconMail.svelte';
- import IconSend from '$lib/components/icons/IconSend.svelte';
- import {Button, Control, Field, FieldErrors, Label} from "$lib/components/ui/form";
- import {Input} from "$lib/components/ui/input";
- import {superForm} from "sveltekit-superforms";
- import {zodClient} from "sveltekit-superforms/adapters";
- import {contactZodSchema} from "@repo/schemas/zod";
- import * as m from "$paraglide/messages";
- import {Textarea} from "$lib/components/ui/textarea";
+    import IconBrandFacebook from '$lib/components/icons/IconBrandFacebook.svelte';
+    import IconBrandInstagram from '$lib/components/icons/IconBrandInstagram.svelte';
+    import {IconMail, IconPhone, IconSend} from '@tabler/icons-svelte';
+    import {Button, Control, Field, FieldErrors, Label} from "$lib/components/ui/form";
+    import {Input} from "$lib/components/ui/input";
+    import {superForm} from "sveltekit-superforms";
+    import {zodClient} from "sveltekit-superforms/adapters";
+    import {contactZodSchema} from "@repo/schemas/zod";
+    import * as m from "$paraglide/messages";
+    import {Textarea} from "$lib/components/ui/textarea";
+    import Form from "$lib/components/base/Form.svelte";
 
- export let data;
+    export let data;
 
  const form = superForm(data.form, {
      validators: zodClient(contactZodSchema),
@@ -21,40 +20,44 @@
  const { form: formData, enhance } = form;
 </script>
 
-<div class="relative container mx-24 my-24 w-full flex flex-row">
-    <div class="absolute min-w-72 min-h-72 rounded-full opacity-5 bg-primary filter blur-3xl left-1/3">
+<div class="relative container py-24 w-full flex flex-row">
+    <div class="absolute w-72 h-72 rounded-full opacity-5 bg-primary filter blur-3xl left-1/3">
 
     </div>
     <div class="flex w-full flex-col gap-8">
         <div class="flex flex-col">
-            <h1 class="text-6xl font-bold">Let's <span class="text-primary">talk</span></h1>
+            <h1 class="text-6xl font-black">Let's <span class="text-primary">talk</span></h1>
             <p class="text-2xl">Ask us anything or just say hi,</p>
         </div>
         <div class="flex flex-col gap-4">
             <div class="flex flex-row items-center gap-4">
-                <IconBrandFacebook />
-                <p class="text-xl">Facebook</p>
+                <div class="w-11 h-11">
+                    <IconBrandFacebook />
+                </div>
+                <p class="text-xl -ml-1">Facebook</p>
             </div>
             <div class="flex flex-row items-center gap-4">
-                <IconBrandInstagram />
-                <p class="text-xl">Instagram</p>
+                <div class="w-11 h-11">
+                    <IconBrandInstagram />
+                </div>
+                <p class="text-xl -ml-1">Instagram</p>
             </div>
             <div class="flex flex-row items-center gap-4">
-                <div class="rounded bg-primary">
-                    <IconPhone/>
+                <div class="rounded-lg bg-primary w-10 h-10 flex items-center justify-center">
+                    <IconPhone stroke="1.5" />
                 </div>
                 <p class="text-xl">07668 996660</p>
             </div>
-            <div class="flex flex-row items-center gap-4">
-                <div class="rounded bg-primary">
-                    <IconMail/>
+            <div class="flex flex-row items-center gap-4 ">
+                <div class="rounded-lg bg-primary w-10 h-10 flex items-center justify-center">
+                    <IconMail stroke="1.5" />
                 </div>
                 <p class="text-xl">booking@we-escape.de</p>
             </div>
         </div>
     </div>
     <section class="container w-full">
-        <form class="flex flex-col gap-6" method="POST" use:enhance>
+        <Form class="flex flex-col gap-6" method="POST" {enhance} {formData}>
             <div class="flex flex-row w-full gap-4">
                 <div class="w-full">
                     <Field {form} name="name">
@@ -86,8 +89,8 @@
                 <p class="text-muted-foreground text-sm">Your message will be copied to support team.</p>
             </div>
             <div class="grid w-full justify-items-end items-end ml-auto">
-                <Button class="flex flex-row gap-2">Send your message  <IconSend/></Button>
+                <Button class="flex flex-row gap-2">Send your message  <IconSend stroke="1.5"/></Button>
             </div>
-        </form>
+        </Form>
     </section>
 </div>
