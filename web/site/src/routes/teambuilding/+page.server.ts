fe0c805b229236ed799032ddfdb842e848ab2400
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-04-17 19:45:42 +0200

    feat: advance on the teambuilding page and add the contact form

--- web/site/src/routes/teambuilding/+page.server.ts
+++ web/site/src/routes/teambuilding/+page.server.ts
@@ -17,8 +17,6 @@ export const actions: Actions = {
     default: async (event) => {
         const form = await superValidate(event, zod(contactTeambuildingZodSchema));
 
-        console.log("salut")
-
         if (!form.valid) {
             return fail(400, {
                 form,
@@ -45,7 +43,7 @@ export const actions: Actions = {
             message,
         });
 
-        await contactTeambuilding.save();   
+        await contactTeambuilding.save();
 
         return redirect(302, route("/"));
     },
