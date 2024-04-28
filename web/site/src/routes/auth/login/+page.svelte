e1f12cb1de861416f0d3880ef1615350c35eb819
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-04-28 13:06:36 +0200

    feat: add translation and responsive for the register and login

--- web/site/src/routes/auth/login/+page.svelte
+++ web/site/src/routes/auth/login/+page.svelte
@@ -1,41 +1,65 @@
 <script lang="ts">
-import {
-	Button,
-	Control,
-	Field,
-	FieldErrors,
-	Label,
-} from "$lib/components/ui/form";
-import { Input } from "$lib/components/ui/input";
-import { userRegisterZodSchema } from "@repo/schemas/zod";
-import { superForm } from "sveltekit-superforms";
-import { zodClient } from "sveltekit-superforms/adapters";
+	import { Field, Control, Label, FieldErrors, Button } from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { userRegisterZodSchema } from '@repo/schemas/zod';
+	import { superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+	import hand from 'site/src/assets/auth/hand.svg';
+	import google_logo from 'site/src/assets/auth/google_logo.svg';
+	import facebook_logo from 'site/src/assets/auth/facebook_logo.svg';
+	import twitter_logo from 'site/src/assets/auth/twitter_logo.svg';
+	import * as m from '$paraglide/messages';
 
-export let data;
+	export let data;
 
-const form = superForm(data.form, {
-	validators: zodClient(userRegisterZodSchema),
-});
+	const form = superForm(data.form, {
+		validators: zodClient(userRegisterZodSchema)
+	});
 
-const { form: formData, enhance } = form;
+	const { form: formData, enhance } = form;
 </script>
 
-<section class="container">
-    <form method="POST" use:enhance>
-        <Field {form} name="email">
-            <Control let:attrs>
-                <Label>Email</Label>
-                <Input {...attrs} bind:value={$formData.email}/>
-            </Control>
-            <FieldErrors/>
-        </Field>
-        <Field {form} name="password">
-            <Control let:attrs>
-                <Label>Password</Label>
-                <Input {...attrs} bind:value={$formData.password}/>
-            </Control>
-            <FieldErrors/>
-        </Field>
-        <Button>Submit</Button>
-    </form>
+<section class="container flex flex-col gap-4">
+	<div class="flex flex-row items-center gap-2">
+		<img src={hand} alt="hand" />
+		<h1 class="text-3xl font-bold md:text-4xl">{m.welcome_back()}</h1>
+	</div>
+	<form method="POST" use:enhance class="flex flex-col gap-8">
+		<Field {form} name="email">
+			<Control let:attrs>
+				<Label class="flex">{m.email()}</Label>
+				<Input {...attrs} bind:value={$formData.email} />
+			</Control>
+			<FieldErrors />
+		</Field>
+		<Field {form} name="password">
+			<Control let:attrs>
+				<Label class="flex">{m.password()}</Label>
+				<Input {...attrs} bind:value={$formData.password} />
+			</Control>
+			<FieldErrors />
+		</Field>
+		<Button
+			class="to-primary my-2 mt-4 w-full bg-gradient-to-r from-orange-500 py-2 text-center text-2xl font-medium"
+			>{m.login()}</Button
+		>
+	</form>
+	<a href="/">
+		<p class="text-muted-foreground text-end underline">{m.forgot_password()}</p>
+	</a>
+
+	<div class="flex flex-row gap-2">
+		<Button
+			class="flex w-full flex-row items-center justify-center gap-1 rounded-md bg-white p-2 text-center"
+		>
+			<img src={google_logo} alt="google_logo" />
+			<span class="text-base font-semibold text-black">{m.login_with_google()}</span>
+		</Button>
+		<Button class="w-fit rounded-md bg-white p-2">
+			<img src={facebook_logo} alt="facebook_logo" />
+		</Button>
+		<Button class="w-fit rounded-md bg-white p-2">
+			<img src={twitter_logo} alt="twitter_logo" />
+		</Button>
+	</div>
 </section>
