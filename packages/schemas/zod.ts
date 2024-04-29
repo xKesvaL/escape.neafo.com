f5dc12d0a0ecf52cecc48a45c2a4fa3717bb68f5
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-04-29 20:53:41 +0200

    feat: add edit profile page and edit user schema to age

--- packages/schemas/zod.ts
+++ packages/schemas/zod.ts
@@ -13,6 +13,7 @@ export type UserRegister = z.infer<typeof userRegisterZodSchema>;
 export const userZodSchema = z.object({
 	_id: z.string(),
 	email: z.string().email(),
+	age: z.number(),
 	firstname: z.string().optional(),
 	lastname: z.string().optional(),
 	role: z.enum(["user", "admin"]).default("user"),
